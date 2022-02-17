6943d33a6d51bf40db8606a38c888a891fd47d2c
Jonaz (BR) <56744848+JonazBR@users.noreply.github.com>
2022-02-17 01:16:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-const express = require('express')
+
+const express = require("express");
+const PORT = process.env.PORT || 8080 || 5000 || 3000
 const app = express()
 
-app.get('/', async(req, res) => {
-res.send('oi')
+
+app.get('/db', async (req, res) => {
+	res.send('oi')
 })
 
 
-app.listen(process.env.port || 8080)
+app.listen(PORT, function() {
+	console.log(`Serve running on port ${PORT}`)
+})
